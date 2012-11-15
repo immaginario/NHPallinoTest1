c07812b0fc8bc84d885b243532ba1796e2260286
Giorgio Bozio <giorgio.bozio@gmail.com>
2012-11-15 09:39:28 +0100

    move schema tests

--- NHPallinoTest1.Test/MappingTests.cs
+++ NHPallinoTest1.Test/MappingTests.cs
@@ -15,25 +15,6 @@ namespace NHPallinoTest1.Test
     [TestFixture]
     public class MappingTests
     {
-        private static void BuildSchema(NHibernate.Cfg.Configuration config)
-        {
-            var path = Path.Combine(
-                AppDomain.CurrentDomain.BaseDirectory,
-                @"script.sql");
-
-            new SchemaExport(config)
-                .SetOutputFile(path)
-                .Create(true, false);
-        }
-
-        [Test]
-        public void CanExportSchemaForMSSql()
-        {
-            Fluently.Configure()
-                .Database(MsSqlConfiguration.MsSql2008.ConnectionString("sgdsfgdfgfd").ShowSql())
-                .Mappings(x => NHHelper.BuildMappings(x))
-                .ExposeConfiguration(BuildSchema)
-                .BuildConfiguration();
-        }
+        
     }
 }
---
+++ NHPallinoTest1.Test/SchemaTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentNHibernate.Cfg;
+using FluentNHibernate.Cfg.Db;
+using NHibernate.Tool.hbm2ddl;
+using NUnit.Framework;
+
+namespace NHPallinoTest1.Test
+{
+    [TestFixture]
+    public class SchemaTests
+    {
+        private static void BuildSchema(NHibernate.Cfg.Configuration config)
+        {
+            var path = Path.Combine(
+                AppDomain.CurrentDomain.BaseDirectory,
+                @"script.sql");
+
+            new SchemaExport(config)
+                .SetOutputFile(path)
+                .Create(true, false);
+        }
+
+        [Test]
+        public void CanExportSchemaForMSSql()
+        {
+            Fluently.Configure()
+                .Database(MsSqlConfiguration.MsSql2008.ConnectionString("sgdsfgdfgfd").ShowSql())
+                .Mappings(x => NHHelper.BuildMappings(x))
+                .ExposeConfiguration(BuildSchema)
+                .BuildConfiguration();
+        }
+    }
+}
