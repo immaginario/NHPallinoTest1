0af30e177afd2a9ef3aea1eb9d18cd757c56470c
Giorgio Bozio <giorgio.bozio@gmail.com>
2012-11-15 10:16:20 +0100

    First mapping test

--- NHPallinoTest1.Test/MappingTests.cs
+++ NHPallinoTest1.Test/MappingTests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -6,7 +7,10 @@ using System.Text;
 using System.Threading.Tasks;
 using FluentNHibernate.Cfg;
 using FluentNHibernate.Cfg.Db;
+using FluentNHibernate.Testing;
+using NHibernate;
 using NHibernate.Tool.hbm2ddl;
+using NHPallinoTest1.Model;
 using NUnit.Framework;
 
 
@@ -15,6 +19,54 @@ namespace NHPallinoTest1.Test
     [TestFixture]
     public class MappingTests
     {
-        
+        [Test]
+        public void CanCorrectlyMapCustomer()
+        {
+            using (ISession session = NHHelper.GetInMemorySession())
+            {
+                new PersistenceSpecification<Customer>(session, new CustomEqualityComparer())
+                    .CheckProperty(c => c.Id, 1)
+                    .CheckProperty(c => c.Address, "via Roma, 1")
+                    .CheckProperty(c => c.Created, new DateTime(2012,1,2))
+                    .CheckProperty(c => c.Name, "Mario")
+                    .CheckProperty(c => c.Surname, "Rossi")
+                    .VerifyTheMappings();
+            }
+        }
+    }
+
+
+    public class CustomEqualityComparer : IEqualityComparer
+    {
+        bool IEqualityComparer.Equals(object x, object y)
+        {
+            if (x == null && y == null)
+            {
+                return true;
+            }
+            if (x == null || y == null)
+            {
+                return false;
+            }
+            if (x is DateTime && y is DateTime)
+            {
+                return ((DateTime)x).ToString() == ((DateTime)y).ToString();
+            }
+            if (x is DateTime? && y is DateTime?)
+            {
+                if (((DateTime?)x).HasValue && ((DateTime?)y).HasValue)
+                    return ((DateTime?)x).Value == ((DateTime?)y).Value;
+                if (!((DateTime?)x).HasValue && !((DateTime?)y).HasValue)
+                    return true;
+                return false;
+            }
+
+            return x.Equals(y);
+        }
+
+        public int GetHashCode(object obj)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- NHPallinoTest1.Test/NHHelper.cs
+++ NHPallinoTest1.Test/NHHelper.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using FluentNHibernate.Cfg;
+using FluentNHibernate.Cfg.Db;
+using NHibernate;
+using NHibernate.Tool.hbm2ddl;
 using NHPallinoTest1.Model;
 using NHPallinoTest1.NHMappings;
 
@@ -16,6 +19,18 @@ namespace NHPallinoTest1.Test
             var mapping = mappingConfiguration.FluentMappings
                             .AddFromAssemblyOf<ShopMapping>();
             return mapping;
+        }
+
+        private static readonly FluentConfiguration config = Fluently.Configure()
+                .Mappings(m => m.FluentMappings.AddFromAssemblyOf<ShopMapping>())
+                .Database(SQLiteConfiguration.Standard.InMemory());
+        
+        public static ISession GetInMemorySession()
+        {
+            var sessionFactory = config.BuildSessionFactory();
+            var session = sessionFactory.OpenSession();
+            new SchemaExport(config.BuildConfiguration()).Execute(false, true, false, session.Connection, null);
+            return session;
         }
     }
 }
